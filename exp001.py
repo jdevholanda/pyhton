0e82110d9554602986c56200db40cb257a54e15e
jdevholanda <127545913+jdevholanda@users.noreply.github.com>
2023-03-10 16:11:01 -0300

    Create exp001.py

---
+++ exp001.py
@@ -0,0 +1,43 @@
+altura = float(input("Qual sua altura ? "))
+peso = float(input("Qual seu peso:  "))
+
+x = altura * altura
+y = peso 
+
+imc = y / x
+
+if imc >= 40:
+    print("__________________")
+    print("O seu IMC é de: ", imc )
+
+    print("Obesidade Grau III")
+    print("__________________")
+
+elif imc < 40 and imc  >= 35:
+    print("__________________")
+    print("O seu IMC é de: ", imc )
+
+    print("Obesidade Grau II")
+    print("__________________")
+
+elif imc >= 30 and imc <= 34.9: 
+    print("__________________")
+    print("O seu IMC é de: ", imc )
+
+    print("Obesidade Grau I")
+    print("__________________")
+
+elif imc >= 18.5 and imc <= 24.9:
+    print("__________________")
+    print("O seu IMC é de: ", imc )
+
+    print("Peso normal")
+    print("__________________")
+
+else: 
+    print("O seu IMC é de: ", imc)
+
+    print("Você é um papel. :)")
+
+
+
